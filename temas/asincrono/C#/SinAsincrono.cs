63e83f3932c9d15caea28e9412b2a71b4329e111
Alejandro <alexguitarte@gmail.com>
2023-05-08 17:32:49 +0200

    Refactoring done to asynchronous example, changed classes and methods names

--- temas/asincrono/C#/SinAsincrono.cs
+++ temas/asincrono/C#/SinAsincrono.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Threading;
 
-public class Asincrono{
+public class SinAsincrono{
 
-    public string ObtenerDatosAsincronos(){
+    public string ObtenerDatos(){
         Thread.Sleep(5000);
         return "Datos obtenidos";
     }
 
     public static void Main(){
-        var asincrono = new Asincrono();
-        var datos = asincrono.ObtenerDatosAsincronos();
+        var asincrono = new SinAsincrono();
+        var datos = asincrono.ObtenerDatos();
         Console.WriteLine("Obteniendo datos...");
 
         Console.WriteLine(datos);
